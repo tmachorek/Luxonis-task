feb009c4cdd91228c8b4b39a1ddd7ae833a91c57
machoto3 <68558213+machoto3@users.noreply.github.com>
2023-07-15 21:48:01 +0200

    [BUGFIX] - Finally fixed that bug

--- Scrapping/Sreality_scrapper/Sreality_scrapper/pipelines.py
+++ Scrapping/Sreality_scrapper/Sreality_scrapper/pipelines.py
@@ -1,6 +1,5 @@
 import psycopg2
 import os
-import time
 
 
 class PostgresPipeline:
--- Scrapping/initialize_spider.py
+++ Scrapping/initialize_spider.py
@@ -1,36 +1,6 @@
-import os
-import time
-import psycopg2
-import logging
-
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from Sreality_scrapper.Sreality_scrapper.spiders.property_spider import PropertySpider
-
-if os.getenv('RUNNING_IN_DOCKER') is not None:
-    host = "postgres-container"
-else:
-    host = "localhost"
-
-conn = psycopg2.connect(
-    host=host,
-    database="postgres",
-    user="postgres",
-    password="postgre",
-    port="5432"
-)
-
-curr = conn.cursor()
-
-while True:
-    curr.execute(f"SELECT to_regclass('SREALITY');")
-    if curr.fetchone()[0] is not None:
-        break
-    else:
-        logging.info("Table does not exist yet. Waiting...")
-        time.sleep(1)
-
-conn.close()
 
 settings = get_project_settings()
 
